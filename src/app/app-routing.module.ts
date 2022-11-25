642b76dd16a01f0aeb1a6b7de9982343f18d0439
Giliard H. da Silva <ghssilva248@gmail.com>
2022-11-25 19:48:56 -0300

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContentComponent } from './pages/content/content.component';
+import { ContentComponent } from './pages/home/content.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
